d0da23b2f880c6768ebbf8b0202f57802fffc0ff
Ugur Koc <43906965+ugurkocde@users.noreply.github.com>
2022-12-11 21:01:19 +0100

    Update Starter_Script.sh

--- Starter_Script.sh
+++ Starter_Script.sh
@@ -20,8 +20,7 @@ sudo curl -LO https://github.com/ugurkocde/MacOSXforIntune/raw/main/CompanyPorta
 # sudo installer -pkg *.pkg -target /
 
 # Remove the downloaded .pkg file
-cd ~/Downloads
-sudo rm /*.pkg
+rm /*.pkg
 echo "$(tput setaf 2)Company Portal installed$(tput sgr0)"
 # Build checks if the company portal is installed
 
