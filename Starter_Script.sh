eb54cbb7208d211f20e4a8e40f2f24fc8b639d75
Ugur Koc <43906965+ugurkocde@users.noreply.github.com>
2022-12-11 19:59:43 +0100

    Update Starter_Script.sh

--- Starter_Script.sh
+++ Starter_Script.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
 sudo nvram boot-args="serverperfmode=1 $(nvram boot-args 2>/dev/null | cut -f 2-)"
+echo "Performance Mode enabled"
 
 # massively increase virtualized macOS by disabling spotlight.
 sudo mdutil -i off -a
+echo "Disabled spotlight"
 
 ## Install Company Portal
 
 # Download the .pkg file using curl
-curl -O https://go.microsoft.com/fwlink/?linkid=853070
+curl -O https://github.com/ugurkocde/MacOSXforIntune/blob/main/CompanyPortal-Installer.pkg
+echo "Downloading and installing Company Portal"
 
 # Install the .pkg file silently using the installer command
 installer -pkg ./*.pkg -target /
 
 # Remove the downloaded .pkg file
 rm ./*.pkg
-
+echo "Company Portal installed"
+# Build checks if the company portal is installed
 
 # User runs this: curl -s https://raw.githubusercontent.com/myusername/myrepo/main/install_pkg.sh | bash
